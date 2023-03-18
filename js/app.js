e1bf1de64514542cba4a416c11da0cef719c2e52
szabiweb21@gmail.com <szabolcssreiber@MacBook-Pro-von-szabolcs.local>
2023-03-18 20:00:12 +0100

    first push

---
+++ js/app.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+if(localStorage.getItem("theme") == "invert"){
+    document.querySelector("body").classList.add("invert"); 
+}
+let currentTime= new Date();
+let year = currentTime.getFullYear();
+document.querySelector("#currentYear").innerHTML= year;
+});
+
+
+
+
+document.querySelector('#invertButton').addEventListener('click', ()=> {
+   document.querySelector('body').classList.toggle("invert");
+
+   if(localStorage.getItem("theme") == "invert") {
+       localStorage.setItem("theme", "")
+   }
+   else {
+    localStorage.setItem("theme", "invert")
+   }
+});
